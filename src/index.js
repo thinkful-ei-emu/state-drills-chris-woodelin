6b81cf105eb9875d5b151e88b70003705e7d6ea6
woodelin <woodelin@woodelins-MBP.home>
2019-06-26 17:10:42 -0400

    great work

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
+
+
 // import HelloWorld from './state-drills/HelloWorld'
 // import Bomb from './state-drills/bomb'
 // import RouletteGun from './state-drills/RouletteGun'
